fe1d3b48ca2635a694ca5b40275935a440b8841f
Trif Sergiu <niculescu95@yahoo.com>
2019-09-23 21:30:37 +0300

    studied enhance for

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -21,8 +21,28 @@ public class Game {
 
         initializeCompetitors();
 
+        // for-each or enhance for loop
+        for (Vehicle vehicle : competitors) {
+            double speed = getAccelerationSpeedFromUser();
+            vehicle.accelerate(60, 1 );
+        }
 
     }
+
+    private double getAccelerationSpeedFromUser () {
+        System.out.println("Please enter acceleration speed:");
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            return scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("You have entered an invalid number.");
+            return getAccelerationSpeedFromUser();
+        }
+
+    }
+
+
 
     private Track getSelectedTrackFromUser() {
         System.out.println("Please select a track");
